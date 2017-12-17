ab44de902be9beaa5383694629a5089110a15073
Fruit of Eden <lu.gt@163.com>
2017-12-17 19:38:35 +0800

    checkout，标题

--- js/move-top.js
+++ js/move-top.js
@@ -7,18 +7,20 @@ function show_top_header(){
 	document.write('<div class="top_bg">\n\t<div class="container">\n\t\t<div class="header_top-sec">\n\t\t  <div class="clearfix"> </div>\n\t\t</div>\n\t</div>\n</div>\n<!-- top-header -->\n<!-- logo-cart -->\n<div class="header_top">\n\t<div class="container">\n\t\t<div class="logo">\n\t\t \t<a href="index.html" id="topCenterTitle">萌多多</a>\t\t\t \n\t\t</div>\n\t\t<div class="header_right">\n\t\t\t<div class="cart box_1">\n\t\t\t\t<a href="checkout.html">\n\t\t\t\t<h3> <div class="total">\n\t\t\t\t\t<span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity">0</span>件)</div>\n\t\t\t\t\t<img src="images/cart1.png" alt=""/></h3>\n\t\t\t\t</a>\n\t\t\t\t<p>'+((location.pathname=="/smartpets/checkout.html")?'<a href="javascript:;" class="simpleCart_empty">清空购物车</a>':'点击查看')+'</p>\n\t\t\t\t<div class="clearfix"> </div>\n\t\t\t</div><div class="cart box_1" style="margin-right: 10px;border-right-color: #f44271; border-right-style: solid;padding-right: 10px;">\n\t\t\t\t<a href="orderlist.html">\n\t\t\t\t<h3> <div class="total">\n\t\t\t\t\t订单</div>\n\t\t\t\t\t</h3>\n\t\t\t\t</a>\n\t\t\t\t<p><a href="orderlist.html">点击查看</a></p>\n\t\t\t\t<div class="clearfix"> </div>\n\t\t\t</div>\t\t\t\t \n\t\t</div>\n\t\t<div class="clearfix"></div>\t\n\t</div>\n</div>\n<!-- //logo-cart -->\n<!------>\n');	
 }
 
-setTimeout(alterText, 15000);
+setTimeout(alterText, 8000);
 var curIndex = 0;
-var titleTexts = ["萌多多","健康","美味","贴心","萌多多"];
+var titleTexts = ["萌多多","健康","美味","宠物专家","萌多多"];
 function alterText(){
-	$("#topCenterTitle").animate({opacity: 'hide'},1500,function(){
+	$("#topCenterTitle").animate({opacity: 'hide'},500,function(){
 		curIndex ++;
 		curIndex = curIndex % titleTexts.length;
 		$("#topCenterTitle").text(titleTexts[curIndex]);
-		if(curIndex === titleTexts.length - 1){
-			setTimeout(alterText, 15000);
-		}else{
-			setTimeout(alterText, 3000);
-		}
+		$("#topCenterTitle").animate({opacity: 'show'},300,function(){
+			if(curIndex === titleTexts.length - 1){
+				setTimeout(alterText, 8000);
+			}else{
+				setTimeout(alterText, 2000);
+			}
+		});
 	});
 }
